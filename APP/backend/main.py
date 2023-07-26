be5f974370968281c2550843561e54a8d873cbd9
leapradier <lefpradier@gmail.com>
2023-07-26 12:18:48 +0200

    first commit

---
+++ APP/backend/main.py
@@ -0,0 +1,40 @@
+import uuid
+import uvicorn
+from fastapi import File
+from fastapi import FastAPI
+from fastapi import UploadFile
+import numpy as np
+import mlflow
+import asyncio
+import spacy_fastlang
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import PlainTextResponse
+
+
+# load model and predict
+loaded_model = mlflow.pyfunc.load_model("model")
+app = FastAPI()
+
+
+# PING
+@app.get("/")
+def read_root():
+    return {"message": "Welcome from the API"}
+
+
+# predict
+@app.post("/tweet")
+async def predict_stm(tweet: str):
+    pred = loaded_model.predict(tweet)
+    score = float(pred[0][0])
+    print(score)
+    return {"score": score}
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, ex):
+    return PlainTextResponse(str(ex), status_code=400)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
---
+++ APP/frontend/main.py
@@ -0,0 +1,52 @@
+import requests
+import streamlit as st
+from PIL import Image
+import matplotlib.pyplot as plt
+
+##! STREAMLITE
+
+
+# # https://discuss.streamlit.io/t/version-0-64-0-deprecation-warning-for-st-file-uploader-decoding/4465
+# st.set_option("deprecation.showfileUploaderEncoding", False)
+
+# defines an h1 header
+st.title("Tweet sentiment analysis web app")
+
+# displays a file uploader widget
+tweet = st.text_input("Import a tweet")
+
+# displays a button
+if st.button("Predict sentiment"):
+    if tweet is not None:
+        data = {"tweet": tweet}
+        res = requests.post("http://backend:8080/tweet", params=data)
+        output = res.json()
+        score = output.get("score")
+        if score >= 0.5:
+            pos_txt = score / 2
+            score_txt = "Négatif à " + str(round(score * 100, 1)) + "%"
+            image = plt.imread("sad.png")
+        else:
+            pos_txt = (1 - score) / 2 + score
+            score_txt = "Positif à " + str(round((1 - score) * 100, 1)) + "%"
+            image = plt.imread("happy.png")
+
+        fig = plt.figure(figsize=(5, 2), facecolor="#0E1117")
+        gs = fig.add_gridspec(nrows=2, ncols=1, height_ratios=[1, 1])
+        axs = {}
+
+        axs["img"] = fig.add_subplot(gs[0, :])
+        im = axs["img"].imshow(image)
+
+        axs["bar"] = fig.add_subplot(gs[1, :])
+        axs["bar"].barh(0, score, color="red")
+        axs["bar"].barh(0, (1 - score), color="green", left=score)
+        axs["bar"].text(
+            pos_txt, 0, score_txt, color="white", fontsize=11, va="center", ha="center"
+        )
+
+        axs["bar"].set_facecolor("#0E1117")
+        axs["bar"].set_axis_off()
+        axs["img"].set_facecolor("#0E1117")
+        axs["img"].set_axis_off()
+        st.pyplot(fig)
